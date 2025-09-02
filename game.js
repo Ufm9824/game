8c478cff9c4bf21e4b6c45ad4a93e0baa8bd883a
Ufm9824 <ufm9824@edu.kunskapsskolan.se>
2025-09-02 14:20:50 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,16 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const ver = document.getElementById('ver');
   const Red = document.getElementById('red');
   const Blue = document.getElementById('blue');
   const Green = document.getElementById('green');
   const check = document.getElementById('check');
 
+  let version = 1.0
+  ver.textContent = version;
+  
   let list = [Red, Blue, Green, check];
 
   for (let i = 0; i < list.length; i++) {
     const now = list[i];
     
     now.addEventListener('click', () => {
-      console.log(i);
+      console.log(i)
     });
+    
   }
 });
