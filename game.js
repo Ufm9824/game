905be185655e133198ea41d4217038df331c16c3
Ufm9824 <ufm9824@edu.kunskapsskolan.se>
2025-09-03 10:29:04 +0200

    Update game.js

--- game.js
+++ game.js
@@ -18,4 +18,36 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     
   }
+  
+  function changeFavicon(src) {
+  let link = document.querySelector("link[rel~='icon']");
+  
+  if (!link) {
+    link = document.createElement("link");
+    link.rel = "icon";
+    document.head.appendChild(link);
+  }
+  
+  link.href = src;
+}
+
+let isFirst = 0
+let i = 0
+
+function gameLoop(isFirst) {
+
+  if(isFirst = 0) {
+  isFirst = 1
+  console.log("First frame")
+  }
+  else{
+  console.log("Frame: " + i)
+  }
+  i++
+  requestAnimationFrame(gameLoop)
+}
+
+// <======
+  
+requestAnimationFrame(gameLoop)
 });
