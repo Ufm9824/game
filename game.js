08c15f4f8af9333a495e461874eff2179d8a57d6
Ufm9824 <ufm9824@edu.kunskapsskolan.se>
2025-09-03 11:20:09 +0200

    Update game.js

--- game.js
+++ game.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const Blue = document.getElementById('Blue');
   const Green = document.getElementById('Green');
   const check = document.getElementById('check');
-
+  const square = document.getElementById('square');
+  
   let version = 1.3;
   ver.textContent = version;
   
@@ -39,24 +40,36 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  let isFirst = true;
   let i = 0;
+  let hJ = 0;
+  let y = 50;
+  let x = 50;
+  let yV = 0;
 
   function gameLoop() {
-    if (isFirst) {
-      isFirst = false;
-      console.log("First frame");
-    } else {
-      console.log("Frame: " + i);
-    }
 
     if (spacePressed) {
-      changeFavicon("jeremy.png");
-    } else {
-      changeFavicon("happy.png");
+      if(hJ == 0) {
+        hJ = 1
+        yV += 5
+    }};
+
+    if( == 50) {
+      hJ = 0
+      yV = 0
     }
 
-    i++;
+    if(yV < 0.5) {
+      yv = 0
+    } else {
+      yv -= yv / 3
+    }
+
+    y += yV
+
+    square.style.left = x + 'px'
+    square.style.top = y + 'px'
+
     requestAnimationFrame(gameLoop);
   }
 
